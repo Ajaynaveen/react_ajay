c187071c81836e73bdcab1c0fd6f92c62e568268
Ajay <ajay@mallow-tech.com>
2023-09-02 00:28:28 +0530

    added the add to cart project

--- src/App.js
+++ src/App.js
@@ -1,119 +1,93 @@
-// Note Taking Application - CRUD Operations
-// Create, Read, Update, Delete
 
-import React, { useEffect, useState } from 'react';
-import Note from './components/Note';
-import axios from 'axios';
+
+import React, { useState } from 'react';
+import './styles.css'; // Import the CSS file
+
+function CartItem({ item, removeFromCart }) {
+  return (
+    <div className="cart-item">
+      <h3>{item.name}</h3>
+      <p>Price: ${item.price}</p>
+      <p>Rating: {item.rating}</p>
+      <button
+        className="remove-button"
+        onClick={() => removeFromCart(item)}
+      >
+        Remove from Cart
+      </button>
+    </div>
+  );
+}
+
+function Product({ item, addToCart }) {
+  return (
+    <div className="product">
+      <h3>{item.name}</h3>
+      <p>Price: ${item.price}</p>
+      <p>Rating: {item.rating}</p>
+      <button className="add-button" onClick={() => addToCart(item)}>
+        Add to Cart
+      </button>
+    </div>
+  );
+}
 
 function App() {
-  const [notes, setNotes] = useState([]);
-  const [newNote, setNewNote] = useState('');
-  const [showAll, setShowAll] = useState(true);
+  const items = [
+    {
+      id: 1,
+      name: 'Product 1',
+      price: 10,
+      rating: 4.5,
+    },
+    {
+      id: 2,
+      name: 'Product 2',
+      price: 15,
+      rating: 3.8,
+    },
+    {
+      id: 3,
+      name: 'Product 3',
+      price: 25,
+      rating: 4.8,
+    },
+    {
+      id: 4,
+      name: 'Product 4',
+      price: 35,
+      rating: 4.2,
+    },
+  
+  ];
 
-  // make the api request
-  useEffect(() => {
-    axios
-      .get('https://be-sa97.onrender.com/api/notes')
-      .then(response => {
-        setNotes(response.data);
-      })
-  }, [notes]);
+  const [cart, setCart] = useState([]);
 
-  const notesToShow = showAll ? notes : notes.filter(note => note.important === true);
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+  };
 
-  // addNote function
-  const addNote = (event) => {
-    event.preventDefault();
-    // console.log('button clicked', event.target);
-    
-    // create the new note object
-    const noteObject = {
-      content: newNote,
-      important: Math.random() < 0.5,
-    };
-
-    // append it to the notes state
-    // setNotes(notes.concat(noteObject));
-    axios
-      .post('https://be-sa97.onrender.com/api/notes', noteObject)
-      .then(response => {
-        // console.log(response);
-        // after a creating a new resource
-        // update the state
-        setNotes(notes.concat(response.data));
-        setNewNote('');
-      });
-
-    // clear the newNote state
-    setNewNote('');
-  }
-
-  // handleNoteChange handler function
-  const handleNoteChange = (event) => {
-    // console.log(event.target.value);
-    setNewNote(event.target.value);
-  }
-
-  const toggleImportanceOf = (id) => {
-    // console.log('importance of ' + id + ' needs to be toggled');
-    const url = `https://be-sa97.onrender.com/api/notes/${id}`;
-    const note = notes.find(n => n.id === id);
-    const changedNote = {
-      ...note,
-      important: !note.important
-    };
-
-    // make the api request
-    axios
-      .put(url, changedNote)
-      .then(response => {
-        setNotes(notes.map(note => {
-          // return the entire original array or
-          // the updated
-          return note.id !== id ? note : response.data;
-        }));
-      });
-  }
-
-  const deleteNoteHandler = (id) => {
-    // console.log(id);
-    const url = `https://be-sa97.onrender.com/api/notes/${id}`;
-    axios
-      .delete(url)
-      .then(response => {
-        setNotes(notes.filter(note => {
-          return note.id !== id;
-        }))
-      })
-  }
+  const removeFromCart = (product) => {
+    const updatedCart = cart.filter((item) => item.id !== product.id);
+    setCart(updatedCart);
+  };
 
   return (
-    <div>
-      <h1>Notes</h1>
-      <div>
-        <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all'}
-        </button>
+    <div className="container">
+      <h1>Items here</h1>
+      <div className="products-container">
+        {items.map((item) => (
+          <Product key={item.id} item={item} addToCart={addToCart} />
+        ))}
       </div>
-      <ul>
-        {
-          notesToShow.map(
-            note => <Note
-              key={note.id}
-              note={note} 
-              toggleImportance={() => toggleImportanceOf(note.id)}
-              deleteNote={() => deleteNoteHandler(note.id)}
-              />
-          )
-        }
-      </ul>
-
-      <form onSubmit={addNote}>
-        <input value={ newNote } onChange={handleNoteChange} placeholder='a new note...'/>
-        <button type='submit'>save</button>
-      </form>
+      <h2>Cart items</h2>
+      <div className="cart-container">
+        {cart.map((item) => (
+          <CartItem key={item.id} item={item} removeFromCart={removeFromCart} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
